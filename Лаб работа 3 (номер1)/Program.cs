47f68172c010d322ea0404f0112f3d70f1c9fd7d
sidnatasha03 <90253059+sidnatasha03@users.noreply.github.com>
2021-09-29 10:03:54 +0300

    Add files via upload

---
+++ Лаб работа 3 (номер1)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Shershnev.Lab3.Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x: ");
+            string xstr = Console.ReadLine();
+            //Конвертируем введённое пользователем значение х в double
+            double x = Convert.ToDouble(xstr);
+            //Задаём условия
+            if (x > 250)
+            {
+                Console.WriteLine(Math.Abs(3 * x - 2));
+            }
+            if (0 <= x & x <= 250)
+            {
+                Console.WriteLine(Math.Abs(4 + x));
+            }
+            if (x < 0)
+            {
+                Console.WriteLine(Math.Abs(5 * x));
+            }
+
+        }
+
+    }
+}
